91c1087517221ba5a3bf9f29417ae5c6c96c919f
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-08-22 22:27:21 +0700

    learn pre fork on go

--- golang-fiber/main.go
+++ golang-fiber/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +12,14 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
+		Prefork: true,
 	})
+
+	if fiber.IsChild(){
+		fmt.Println("I'm child proccess")
+	} else {
+		fmt.Println("I'm parent proccess")
+	}
 
 	err := app.Listen(":3000")
 	if err != nil {
